94e49425777b2f1803bcff83bb6a12d0e47f6b33
Andrew Dawdy <axd6837@rit.edu>
2018-10-10 19:31:38 -0400

    DNS Emumeration (number 2)
    Added the script file (DNS_enumeration.bat), the input file needed (input_hostnames.txt) and the output file (Addresses.txt)

---
+++ DNS Enumeration/DNS_enumeration.bat
@@ -0,0 +1,7 @@
+@echo off
+set hostnames=input_hostnames.txt
+FOR /F %%i in (%hostnames%) do call :lookup %%i
+goto :eof
+:lookup
+set srv=%1
+FOR /f "tokens=2" %%j in ('nslookup %srv%^|find /i "Address"') do echo %srv% - %%b >> Addresses.txt
