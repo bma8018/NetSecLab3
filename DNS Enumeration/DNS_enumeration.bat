a0a2268a32da43b0d407c4b14f1360c94ec67854
adawdy <gambitsttg@gmail.com>
2018-10-10 23:06:26 -0400

    bug fix for ipv6 addresses

--- DNS Enumeration/DNS_enumeration.bat
+++ DNS Enumeration/DNS_enumeration.bat
@@ -4,4 +4,4 @@ FOR /F %%i in (%hostnames%) do call :lookup %%i
 goto :eof
 :lookup
 set srv=%1
-FOR /f "tokens=2" %%j in ('nslookup %srv%^|find /i "Address"') do echo %srv% - %%j >> Addresses.txt
+FOR /f "tokens=3" %%j in ('nslookup %srv%^|find /i "Addresses"') do echo %srv% - %%j >> Addresses.txt
