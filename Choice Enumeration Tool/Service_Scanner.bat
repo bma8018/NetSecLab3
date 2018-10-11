151399f9a9a27946a232ee69e082a26ded60de5c
Andrew Dawdy <axd6837@rit.edu>
2018-10-11 15:34:30 -0400

    hotfix

--- Choice Enumeration Tool/Service_Scanner.bat
+++ Choice Enumeration Tool/Service_Scanner.bat
@@ -4,5 +4,5 @@ FOR /F %%i in (%hostnames%) do call :lookup_service %%i
 goto :eof
 :lookup_service
 set machine=%1
-FOR /f (tokens=1*) %%j in ('systeminfo /s %machine%^|findstr /B /C:"OS Version"') do echo %machine% - %%j >> system_info.txt
+FOR /f (tokens=*) %%j in ('systeminfo /s %machine%^|findstr /B /C:"OS Version"') do echo %machine% - %%j >> system_info.txt
 
