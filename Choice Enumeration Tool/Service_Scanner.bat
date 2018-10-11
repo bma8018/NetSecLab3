7df139cfc4892496de46e722ce1ecfca3c530f16
adawdy <gambitsttg@gmail.com>
2018-10-11 00:06:44 -0400

    bug fix

--- Choice Enumeration Tool/Service_Scanner.bat
+++ Choice Enumeration Tool/Service_Scanner.bat
@@ -4,5 +4,5 @@ FOR /F %%i in (%hostnames%) do call :lookup_service %%i
 goto :eof
 :lookup_service
 set machine=%1
-FOR /f "tokens=2" %%j in ('systeminfo /s %machine%^|find /i "OS Version"') do echo %machine% - %%j >> system_info.txt
+FOR /f "tokens=3" %%j in ('systeminfo /s %machine%^|find /i "OS Version"') do echo %machine% - %%j >> system_info.txt
 
